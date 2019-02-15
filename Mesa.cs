a2e0898092a21d4b2db846356c645ffffbfb5452
Flávio Francisco Messias Pedrosa <525969@sga.pucminas.br>
2019-02-14 22:03:54 -0200

    cartas e mesas criados

---
+++ Mesa.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Devops
+{
+   
+    public class Mesa{
+        public Mesa( Mao maoEsquerda, Mao maoDireita)
+        {
+            this._maoDireita = maoDireita;
+            this._maoEsquerda = maoEsquerda;
+        }
+
+        private Mao _maoEsquerda;
+        private Mao _maoDireita;
+
+        //Regras
+    }    
+    
+   
+}
