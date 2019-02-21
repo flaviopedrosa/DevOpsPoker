e56fcda7e5926010c53690173ac41ba8f06b4940
Flávio Francisco Messias Pedrosa <525969@sga.pucminas.br>
2019-02-21 20:18:08 -0300

    adicionado valor a mao e comentario das regras a serem implementadas

--- Mesa.cs
+++ Mesa.cs
@@ -14,6 +14,16 @@ namespace Devops
         private Mao _maoDireita;
 
         //Regras
+        //Carta Alta: A carta de maior valor.
+        //Um Par: Duas cartas do mesmo valor.
+        //Dois Pares: Dois pares diferentes.
+        //Trinca: Três cartas do mesmo valor e duas de valores diferentes.
+        //Straight (sequência): Todas as cartas com valores consecutivos.
+        //Flush: Todas as cartas do mesmo naipe.
+        //Full House: Uma trinca e um par.
+        //Quadra: Quatro cartas do mesmo valor.
+        //Straight Flush: Todas as cartas são consecutivas e do mesmo naipe.
+        //Royal Flush: A seqüência 10, Valete, Dama, Rei, Ás, do mesmo naipe.
     }    
     
    
